a04b4600044e40c213450d4b5f274da7ed126d63
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2018-03-29 13:44:08 -0600

    created tree

---
+++ src/drawing/view/ArtPanel.java
@@ -0,0 +1,122 @@
+package drawing.view;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import drawing.controller.DrawingController;
+
+public class ArtPanel extends JPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private DrawingController app;
+	
+	private SpringLayout appLayout;
+	private ShapeCanvas canvas;
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton clearButton;
+	private JButton saveButton;
+	private JButton colorButton;
+	
+	private int currentEdgeCount;
+	private int currentScale;
+	
+	public ArtPanel(DrawingController app)
+	{
+		super();
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MAXIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		
+		canvas = new ShapeCanvas(app);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0, 1));
+		
+		triangleButton = new JButton("add triangle");
+		rectangleButton = new JButton("add rectangle");
+		ellipseButton = new JButton("add ellipse");
+		polygonButton = new JButton("add polygon");
+		clearButton = new JButton("clear image");
+		saveButton = new JButton("save image");
+		colorButton = new JButton("change color");
+		
+		setupSliders();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+
+
+
+	}
+	
+	private void setupSliders()
+	{
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		Hashtable<Integer, JLabel> edgeLabels = new Hashtable<Integer, JLabel>();
+
+		scaleLabels.put(MINIMUM_SCALE, new JLabel("<HTML>Small<BR>Shape</JTML>"));
+		scaleLabels.put((MAXIMUM_SCALE + MINIMUM_SCALE) / 2, new JLabel("<HTML>Small<BR>Shape</JTML>"));
+		scaleLabels.put(MAXIMUM_SCALE, new JLabel("<HTML>Small<BR>Shape</JTML>"));
+		
+		edgeLabels.put(MINIMUM_EDGE, new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXIMUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+
+		
+		scaleSlider.setLabelTable(scaleLabels);
+		scaleSlider.setOrientation(JSlider.VERTICAL);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+		scaleSlider.setPaintTicks(true);
+		scaleSlider.setPaintLabels(true);
+		
+		edgeSlider.setLabelTable(edgeLabels);
+		edgeSlider.setOrientation(JSlider.VERTICAL);
+		edgeSlider.setSnapToTicks(true);
+		edgeSlider.setMajorTickSpacing(3);
+		edgeSlider.setMinorTickSpacing(1);
+		edgeSlider.setPaintTicks(true);
+		edgeSlider.setPaintLabels(true);
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setBackground(Color.DARK_GRAY);
+		this.setPreferredSize(new Dimension(1024, 768));
+		this.add(canvas);
+		
+		buttonPanel.setPreferredSize(new Dimension(200, 450));
+		buttonPanel.add(triangleButton);
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(ellipseButton);
+		buttonPanel.add(polygonButton);
+		buttonPanel.add(clearButton);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(colorButton);
+		
+		sliderPanel.setPreferredSize(new Dimension(250, 450));
+		sliderPanel.add(scaleSlider);
+		sliderPanel.add(edgeSlider);
+		
+		this.add(buttonPanel);
+		this.add(sliderPanel);
+	}
+
+}
---
+++ src/drawing/view/ShapeCanvas.java
@@ -0,0 +1,37 @@
+package drawing.view;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.*;
+import javax.imageio.*;
+import javax.swing.*;
+
+import drawing.controller.DrawingController;
+
+public class ShapeCanvas extends JPanel
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellipse2D> ellipsList;
+	private ArrayList<Rectangle> rectangleList;
+	private DrawingController app;
+	
+	private BufferedImage canvasImage;
+	
+	public ShapeCanvas(DrawingController app)
+	{
+		super();
+		this.app = app;
+		triangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		rectangleList = new ArrayList<Rectangle>();
+		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(600, 600));
+		this.setMaximumSize(getPreferredSize());
+	}
+
+}
